2e998295dc018cacce18c56765a2934c2f326853
Mukhametzhan <zhanbekovm@mail.ru>
2024-04-08 16:25:08 +0500

    Added exception handling to task-01.py

--- task-01.py
+++ task-01.py
@@ -1,11 +1,21 @@
-number = '124143243456546'
-even_sum = 0
-odd_sum = 0
+try:
+    number = input('Введите число от 0 до 10^20\n')
+    even_sum = 0
+    odd_sum = 0
 
-for digit in number:
-    if int(digit) % 2 == 0:
-        even_sum += int(digit)
+    number = int(number)
+
+    if 0 <= number <= 10**20:
+        for digit in str(number):
+            if int(digit) % 2 == 0:
+                even_sum += int(digit)
+            else:
+                odd_sum += int(digit)
+
+        print(f'Сумма нечетных чисел: {odd_sum} Сумма четных чисел: {even_sum}')
     else:
-        odd_sum += int(digit)
-
-print(f'Сумма нечетных чисел: {odd_sum} Сумма четных чисел: {even_sum}')
+        print("Число должно быть в диапазоне от 0 до 10^20.")
+except ValueError:
+    print("Ошибка: введено неверное значение.")
+except KeyboardInterrupt:
+    print("Процесс прерван.")
