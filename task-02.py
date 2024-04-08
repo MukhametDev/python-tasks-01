e2078818154d9f455269e2a960fda23bbd800dc7
Mukhametzhan <zhanbekovm@mail.ru>
2024-04-08 16:28:31 +0500

    Added exception handling to task-02.py

--- task-02.py
+++ task-02.py
@@ -1,16 +1,24 @@
-number = 10
-count = 1
-arr = []
-index = 0
-while count <= number:
-    arr.append(count)
-    count += 1
+try:
+    number = int(input('Введите число от 1 до 20\n'))
+    count = 1
+    arr = []
+    index = 0
 
-for dig in range(len(arr)):
-    dig = 0
-    while dig != arr[index]:
-        print('#', end="")
-        dig+=1
-    dig = 0
-    index += 1
-    print()
+    if number < 1 or number > 20:
+        print('Введите корректное число')
+    else:
+        while count <= number:
+            arr.append(count)
+            count += 1
+
+        for dig in range(len(arr)):
+            dig = 0
+            while dig != arr[index]:
+                print('#', end="")
+                dig += 1
+            dig = 0
+            index += 1
+            print()
+
+except ValueError:
+    print('Ошибка: введено некорректное значение.')
